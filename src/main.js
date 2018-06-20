0e06f0071d2606f9a633a7ff599a82d389834ed3
NiceNelg <472713985@qq.com>
2018-06-20 13:32:25 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+// 引入自适应css文件
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
+
+//引入快速点击
+import fastclick from 'fastclick';
+
+Vue.config.productionTip = false;
+fastclick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
