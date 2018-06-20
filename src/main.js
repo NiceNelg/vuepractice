145d06263a85bb6ad57e13e2954424364751cd8b
NiceNelg <472713985@qq.com>
2018-06-20 23:11:02 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 // 引入自适应css文件
 import './assets/styles/reset.css';
 import './assets/styles/border.css';
 import './assets/styles/iconfont.css';
+import 'swiper/dist/css/swiper.css';
 
 //引入快速点击
 import fastclick from 'fastclick';
 
 Vue.config.productionTip = false;
 fastclick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption"  ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide>
+        <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180614_CPM_WAP_DE10841.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180614_CPM_WAP_DE10841.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180614_CPM_WAP_DE10841.jpg">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        
+      }
+    }
+  }
+};
+</script>
+
+
+<style lang="stylus" scoped>
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 31.25%
+    .swiper-img
+      width 100%
+</style>
