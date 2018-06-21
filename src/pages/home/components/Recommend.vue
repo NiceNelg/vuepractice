ee94b079f7d6f698f14a4fa5d51044bfbe713743
NiceNelg <472713985@qq.com>
2018-06-21 22:50:17 +0800

    update

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="title">热销组件</div>
+    <ul>
+      <li 
+        class="item border-bottom"
+        v-for="item in recommendList"
+        :key="item.id"
+      >
+        <img class="item-img" :src="item.imgUrl" alt="" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data() {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '第三方士大夫的的方式发多少'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '第三方士大夫的的方式发多少'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '第三方士大夫的的方式发多少'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '第三方士大夫的的方式发多少'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .4rem
+        font-size .32rem
+        elipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        elipsis()
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #fff
+</style>
