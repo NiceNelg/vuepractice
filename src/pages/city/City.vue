744bed5a64972d175554c888ee6fee3971c1a19c
NiceNelg <472713985@qq.com>
2018-06-26 13:28:07 +0800

    update

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search :cities="cities"></city-search>
+    <city-search></city-search>
     <city-list
       :cities="cities"
       :hot="hotCities"
-      :letter="letter"
     ></city-list>
     <city-alphabet
       :cities="cities"
-      @change="handleLetterChange"
     ></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import CityHeader from './components/Header';
 import CitySearch from './components/Search';
 import CityList from './components/List';
@@ -31,8 +30,7 @@ export default {
   data() {
     return {
       cities: {},
-      hotCities: [],
-      letter: ''
+      hotCities: []
     };
   },
   methods: {
@@ -46,9 +44,6 @@ export default {
         this.cities = data.cities;
         this.hotCities = data.hotCities;
       }
-    },
-    handleLetterChange(letter) {
-      this.letter = letter;
     }
   },
   mounted() {
