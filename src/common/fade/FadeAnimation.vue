27ee2d62a4da7f9e30939cdf97b52dc205e5fb41
NiceNelg <472713985@qq.com>
2018-07-04 19:27:38 +0800

    update

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,25 @@
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'FadeAnimation',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .v-enter, .v-leave-to
+    opacity 0
+  .v-enter-active, v-leave-active
+    transition opacity 0.5s
+</style>
